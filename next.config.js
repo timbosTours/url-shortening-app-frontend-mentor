7e98e86736560d5ec23874aefb77ab885f416638
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-02-13 21:47:24 +1100

    setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+import { join } from 'path'
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [join(__dirname, 'styles')],
+  }
 }
 
-module.exports = nextConfig
+export default nextConfig
