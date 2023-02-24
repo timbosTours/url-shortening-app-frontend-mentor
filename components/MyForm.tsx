01e647f73bf749feba49a420d8b99126ad26ab4e
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-02-24 16:42:33 +1100

    basic react-hook-form

--- components/MyForm.tsx
+++ components/MyForm.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function MyForm() {
+  const {register, handleSubmit} = useForm()
+
   return (
-    <form>
-      <input type="text" placeholder="Shorten a link here..." />
+    <form onSubmit={handleSubmit((data) => {
+      console.log(data)
+    })}> 
+      <input {...register("text") }placeholder="Shorten a link here" />
       <button type="submit">Shorten It!</button>
     </form>
   )
