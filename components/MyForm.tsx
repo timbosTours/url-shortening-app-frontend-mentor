701b008176fe8935d6ecd69547354c7570aa5f09
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-03-02 20:55:34 +1100

    useRef to render search link

--- components/MyForm.tsx
+++ components/MyForm.tsx
@@ -2,9 +2,11 @@ import React, { useRef } from 'react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 
+
 function MyForm() {
   const { register, handleSubmit } = useForm()
-  const shortLink = useRef<undefined>()
+  let searchLink = useRef<undefined>()
+  let shortLink = useRef<null>()
 
   return (
     <>
@@ -12,6 +14,7 @@ function MyForm() {
         
         try {
           const resp = await axios.post(`https://api.shrtco.de/v2/shorten?url=${data.text}`);
+          searchLink.current = data.text
           shortLink.current = resp.data.result.short_link
       } catch (error: any) {
         console.log(error)
@@ -20,8 +23,13 @@ function MyForm() {
       <input {...register("text") } placeholder="Shorten a link here" />
       <button type='submit'>Shorten It!</button>
       </form>
+      
       {shortLink.current && 
-        <p>{ shortLink.current }</p> 
+      
+          <p>{shortLink.current}</p>  
+      }
+      {searchLink.current && 
+        <p>{ searchLink.current }</p>
       }
     </>
   )
