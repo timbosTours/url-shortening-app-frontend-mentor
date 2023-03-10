eab5c7636948fd82230b51e5145724b2cfd52258
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-03-10 19:14:12 +1100

    Data interface

--- components/MyForm.tsx
+++ components/MyForm.tsx
@@ -1,24 +1,36 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 
 
+interface Data {
+  input: string
+  output: string
+}
+
 function MyForm() {
   const { register, handleSubmit } = useForm()
-  let searchLink = useRef<string>()
-  let shortLink = useRef<string>()
+  let searchLink = useRef<string>("")
+  let shortLink = useRef<string>("")
+  const [data, setData] = useState<Data[]>([])
 
+  // useEffect(() => {
+  //   window.localStorage.getItem("SEARCH_LINK")
+  // }, [shortLink])
+
+    useEffect(() => {
+    const storedData = localStorage.getItem("data");
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+  }, []);
 
   function setStore() {
       window.localStorage.setItem("SEARCH_LINK", JSON.stringify(searchLink.current))
       window.localStorage.setItem("SHORT_LINK", JSON.stringify(shortLink.current))
   }
 
-  // useEffect(() => {
-  //   const storedLinks = window.localStorage.getItem("SEARCH_LINK")
-  // }, [searchLink])
 
-  
   return (
 
     <>
@@ -28,6 +40,7 @@ function MyForm() {
           const resp = await axios.post(`https://api.shrtco.de/v2/shorten?url=${data.text}`);
           searchLink.current = data.text
           shortLink.current = resp.data.result.short_link
+          setData([])
       } catch (error: any) {
         console.log(error)
       }
@@ -35,8 +48,9 @@ function MyForm() {
       <input {...register("text") } placeholder="Shorten a link here" />
         <button type='submit' onClick={setStore}>Shorten It!</button>
       </form>
-      
-      {shortLink && 
+
+
+      {data && 
       
           <p>{shortLink.current}</p>  
       }
