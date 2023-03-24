2e16aaca48ce7548d0fa6746757da6d3e8ba40d0
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-03-25 10:08:36 +1100

    responsive

--- components/Navbar/HamburgerIcon.tsx
+++ components/Navbar/HamburgerIcon.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import DropDownMenu from '../DropdownMenu/DropDownMenu'
+import DropDownMenu from '../DropdownMenu/DropdownMenu'
 import styles from './Navbar.module.scss'
 
 
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -11,12 +11,16 @@ function Navbar() {
         <nav className={styles.navbar}>
             <Link href="/"><h1>Shortly</h1></Link>
             <HamburgerIcon/>
-            <ul className={styles.mainNavLinks}>
+                <ul className={styles.mainNavLinks}>
+                    <div className={styles.navWrapper}>
                 <li><Link href="/">Features</Link></li>
-                <li><Link href="/">Pricing</Link></li>
+                        <li><Link href="/">Pricing</Link></li>
                 <li><Link href="/">Resources</Link></li>
+                    </div>
+                    <div className={styles.navWrapper}>
                 <li><Link href="/">Login</Link></li>
-                <li><Link href="/">Sign Up</Link></li>
+                        <li><Link href="/">Sign Up</Link></li>
+                    </div>
             </ul>
             </nav>
         </>
