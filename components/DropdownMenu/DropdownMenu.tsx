6f31ffc05fcfec383877381f5b3e1bea75897ad0
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-03-24 20:55:42 +1100

    drop menu styled

--- components/DropdownMenu/DropdownMenu.tsx
+++ components/DropdownMenu/DropdownMenu.tsx
@@ -10,7 +10,7 @@ function DropDownMenu() {
                 <li><Link href="/">Features</Link></li>
                 <li><Link href="/">Pricing</Link></li>
                     <li className={styles.resources}><Link href="/">Resources</Link></li>
-                <li className={styles.loginButton}><Link href="/">Login</Link></li>
+                <li><Link href="/">Login</Link></li>
                 <span><li className={styles.signUpButton}><Link href="/">Sign Up</Link></li></span>
                 </ul>
             </nav>
