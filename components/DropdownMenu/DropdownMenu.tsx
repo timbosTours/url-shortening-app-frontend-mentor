77d0d6705771d4b89528d87b3f07e08a66588929
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-04-25 09:22:25 +1000

    few minor style fixes

--- components/DropdownMenu/DropdownMenu.tsx
+++ components/DropdownMenu/DropdownMenu.tsx
@@ -11,7 +11,7 @@ function DropDownMenu() {
                 <li><Link href="/">Pricing</Link></li>
                     <li className={styles.resources}><Link href="/">Resources</Link></li>
                 <li><Link href="/">Login</Link></li>
-                <span><li className={styles.signUpButton}><Link href="/">Sign Up</Link></li></span>
+                <span><li ><button className={styles.signUpButton}>Sign Up</button></li></span>
                 </ul>
             </nav>
         </div>
